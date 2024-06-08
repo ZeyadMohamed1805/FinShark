0f3ff3db71f7f5477496e63bc6b0a1f4c9b15b2d
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-08 22:11:19 +0300

    Completed The Stocks Controller

---
+++ backend/Controllers/StockController.cs
@@ -0,0 +1,32 @@
+using backend.Data;
+using backend.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public StockController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _dbContext.Stocks.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var stock = _dbContext.Stocks.Find(id);
+            if (stock == null)
+                return NotFound();
+            return Ok(stock);
+        }
+    }
+}
