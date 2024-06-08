f051593c93b0e526ed4fef61289c7bbe83227314
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-08 22:54:19 +0300

    Completed The Stock Controller

--- backend/Controllers/StockController.cs
+++ backend/Controllers/StockController.cs
@@ -1,6 +1,10 @@
 using backend.Data;
+using backend.Dtos.Stock;
 using backend.Mappers;
+using backend.models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 namespace backend.Controllers
 {
@@ -27,6 +31,44 @@ namespace backend.Controllers
             if (stock == null)
                 return NotFound();
             return Ok(stock.ToStockDto());
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFromCreateDTO();
+            _dbContext.Stocks.Add(stockModel);
+            _dbContext.SaveChanges();
+            return Ok(stockModel);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _dbContext.Stocks.FirstOrDefault(stock => stock.Id == id);
+            if (stockModel == null)
+                return NotFound();
+
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _dbContext.SaveChanges();
+
+            return Ok(stockModel);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stock = _dbContext.Stocks.Find(id);
+            if (stock == null)
+                return NotFound();
+
+            _dbContext.Stocks.Remove(stock);
+            _dbContext.SaveChanges();
+
+            return Ok();
         }
     }
 }
